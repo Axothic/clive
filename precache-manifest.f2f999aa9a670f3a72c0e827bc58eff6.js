87ac4ac0ee697aea762503529c911e3c9ca1c7fa
axothic <axothic@gmail.com>
2020-09-21 16:24:00 +0200

    Push

---
+++ precache-manifest.f2f999aa9a670f3a72c0e827bc58eff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ce9be460477e71aaf22ebbaae0bd6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "501cce2a1782de33e5d2",
+    "url": "/static/css/main.4835a748.chunk.css"
+  },
+  {
+    "revision": "8a4757593c0aa1fc1af9",
+    "url": "/static/js/2.31690f48.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.31690f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501cce2a1782de33e5d2",
+    "url": "/static/js/main.e7bf2c5b.chunk.js"
+  },
+  {
+    "revision": "bb5034086d3769d0457d",
+    "url": "/static/js/runtime-main.73eabe50.js"
+  }
+]);
